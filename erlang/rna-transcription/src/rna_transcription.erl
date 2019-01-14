7cf5dd802bb0f5486aef115503916555f046ae7c
agschaid <axel.gschaider@posteo.de>
2019-01-14 22:37:29 +0100

    rna-transcription
    also removed .exercism folders from .github

---
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -0,0 +1,14 @@
+-module(rna_transcription).
+
+-export([to_rna/1]).
+
+
+to_rna(Strand) -> [to_rna_nucleotide(N) || N <- Strand].
+
+to_rna_nucleotide($G) -> $C;
+to_rna_nucleotide($C) -> $G;
+to_rna_nucleotide($T) -> $A;
+to_rna_nucleotide($A) -> $U.
+
+
+
