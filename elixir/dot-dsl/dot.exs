8038398e2afaff1e18696c32bf0ab631e65d790f
Axel Gschaider <axel.gschaider@posteo.de>
2020-12-31 03:18:28 +0100

    elixir dot-dsl: basic framework. empty graph works

--- elixir/dot-dsl/dot.exs
+++ elixir/dot-dsl/dot.exs
@@ -4,5 +4,21 @@ end
 
 defmodule Dot do
   defmacro graph(ast) do
+    lines = case ast[:do] do
+      {:__block__, _meta, l} -> l
+      x -> [x]
+    end
+
+    g = Enum.reduce(lines, %Graph{}, &handle_line/2)
+    Macro.escape(g)
+  end
+
+  defp handle_line(_line, graph) do
+    graph
+  end
+
+  defp add_to(g, t, v) do 
+    new_list = [v|g[t]]
+    # %{g | t new_list}
   end
 end
