b24166beda5248ec2f8e8bb65884d6eff21c9c48
Axel Gschaider <axel.gschaider@posteo.de>
2021-01-01 18:08:57 +0100

    elixir dot-dls: fix all tests

--- elixir/dot-dsl/dot.exs
+++ elixir/dot-dsl/dot.exs
@@ -9,15 +9,36 @@ defmodule Dot do
       x -> [x]
     end
 
-    g = Enum.reduce(lines, %Graph{}, &handle_line/2)
-    Macro.escape(g)
+    sort_by_key = &(List.keysort(&1, 0))
+
+    Enum.reduce(lines, %Graph{}, &handle_line/2)
+    # sorting the lists only so they match the test data
+    |> update(:attrs, sort_by_key)
+    |> update(:edges, sort_by_key)
+    |> update(:nodes, sort_by_key)
+    |> Macro.escape()
+  end
+
+  defp assert_keyword_list(l, context) do
+    if ! Keyword.keyword?(l), do:
+      raise ArgumentError, message: "arguments '#{Macro.to_string(l)}' of #{context} is not a keyword list"
   end
 
   defp handle_line({:graph, _meta, [vals]}, graph), do: add_list(graph, :attrs, vals)
-  defp handle_line({:--, _m1, [ {a,_m2,nil}, {b,_m3,nil  } ]}, graph), do: add_single(graph, :edges, {a,b,[]})
-  defp handle_line({:--, _m1, [ {a,_m2,nil}, {b,_m3,attrs} ]}, graph), do: add_single(graph, :edges, {a,b,attrs})
+  defp handle_line(line={:--, _m1, [ {a,_m2,nil}, {b,_m3,attrs} ]}, graph) do 
+    attrs = case attrs do
+      nil  -> [] # no attributes given
+      [a]  -> a  # attributes given as list
+      _    -> attrs
+    end
+    assert_keyword_list(attrs, "edge '#{Macro.to_string(line)}'")
+    add_single(graph, :edges, {a,b,attrs})
+  end
   defp handle_line({n, _m, nil}, graph), do: add_single(graph, :nodes, {n, []})
-  defp handle_line({n, _m, [attrs]}, graph), do: add_single(graph, :nodes, {n, attrs})
+  defp handle_line({n, _m, [attrs]}, graph) do
+    assert_keyword_list(attrs, "node #{n}")
+    add_single(graph, :nodes, {n, attrs})
+  end
   defp handle_line(l, _g), do: 
     raise ArgumentError, message: "'#{Macro.to_string(l)}' is not a valid dot expression"
 
