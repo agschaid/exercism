bd9f731bf12427d440820a6c7c8d580d395a205e
Axel Gschaider <axel.gschaider@posteo.de>
2021-11-28 22:14:59 +0100

    elixir high_score: implementation

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,25 +1,26 @@
 defmodule HighScore do
-  def new() do
-    # Please implement the new/0 function
-  end
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  @default_score 0
+
+  def new(), do: %{}
+
+  def add_player(scores, name, score \\ @default_score) do
+    Map.put_new(scores, name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    Map.put(scores, name, @default_score)
   end
 
-  def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+  def update_score(scores, name, additional_score) do
+    Map.update(scores, name, @default_score + additional_score, fn score -> score + additional_score end)
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
