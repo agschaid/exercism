585979b755b1083f31c8a05c600a9565270b76a2
Axel Gschaider <axel.gschaider@posteo.de>
2021-11-28 21:02:04 +0100

    elixir guessing_game: fix suggestions

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,8 +1,9 @@
 defmodule GuessingGame do
+  def compare(secret_number, guess \\ :no_guess)
   def compare(x, x) do
     "Correct"
   end
-  def compare(secret_number, guess \\ :no_guess) when guess == :no_guess do
+  def compare(_secret_number, :no_guess) do
     "Make a guess"
   end
   def compare(secret_number, guess) when abs(secret_number-guess) == 1 do
