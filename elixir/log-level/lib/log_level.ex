ac0b76baff540db62825c5ddcee575a40aa90575
Axel Gschaider <axel.gschaider@posteo.de>
2021-11-28 18:40:10 +0100

    elixir log_level: implement

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,23 @@
 defmodule LogLevel do
-  def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+
+  @legacy_codes %{ 1 => :debug, 2 => :info, 3 => :warning, 4 => :error }
+
+  @new_codes Map.merge( @legacy_codes, %{0 => :trace, 5 => :fatal} )
+
+  def to_label(level, true) do
+    Map.get(@legacy_codes, level, :unknown)
+  end
+  def to_label(level, false) do
+    Map.get(@new_codes, level, :unknown)
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    case {to_label(level, legacy?), legacy?} do
+      {:error, _} -> :ops
+      {:fatal, _} -> :ops
+      {:unknown, true} -> :dev1
+      {:unknown, false} -> :dev2
+      _ -> false
+    end
   end
 end
