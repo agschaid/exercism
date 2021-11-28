2f55852c3335f7d5f80fe6233a6a3f8859c9311f
Axel Gschaider <axel.gschaider@posteo.de>
2021-11-28 11:09:40 +0100

    better documentation and delete function

--- elixir/bank-account/lib/bank_account.ex
+++ elixir/bank-account/lib/bank_account.ex
@@ -1,6 +1,7 @@
 defmodule BankAccount do
   @moduledoc """
   A bank account that supports access from multiple processes.
+
   """
 
   @typedoc """
@@ -32,10 +33,23 @@ defmodule BankAccount do
 
   @doc """
   Close the bank. Makes the account unavailable.
+
+  Be aware that this does NOT terminate the underlying process. This helps to
+  differentiate non-existing/deleted accounts from closed ones.
   """
   @spec close_bank(account) :: none
   def close_bank(account) do
     Agent.get_and_update(account, fn s -> manage_account(s, :close) end)
+  end
+
+  @doc """
+  Deletes the bank.
+
+  Be aware that this frees the PID of the bank to be (theoretically) reused in
+  the future. Client code is responsible for sweeping all references to this account.
+  """
+  def delete_bank(account) do
+    Agent.stop(account)
   end
 
   @doc """
