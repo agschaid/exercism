55547c96e3b419d2ff8eedaf0c9cad3bf429c40a
Axel Gschaider <axel.gschaider@posteo.de>
2022-01-15 01:00:43 +0100

    solve high_school_sweetheart

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,36 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    name |> String.trim() |> String.slice(0, 1)
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    ( name |> first_letter() |> String.upcase() ) <> "." 
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [a, b] = full_name |> String.trim() |> String.split()
+    "#{initial(a)} #{initial(b)}"
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{i1}  +  #{i2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
 
-    # Please implement the pair/2 function
   end
 end
