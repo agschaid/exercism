43c4d722bc10f11dbf4234cdbf6bdfb4bdfb85ea
Axel Gschaider <axel.gschaider@posteo.de>
2023-01-13 19:50:33 +0100

    some elixir stuff. not sure it works

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -5,42 +5,60 @@ defmodule RationalNumbers do
   Add two rational numbers
   """
   @spec add(a :: rational, b :: rational) :: rational
-  def add(a, b) do
+  def add({num_a, denom_a}, {num_b, denom_b}) do
+    {
+      (num_a * denom_b) + (num_b * denom_a),
+      denom_a * denom_b
+    }
+    |> reduce()
   end
 
   @doc """
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract(a, b) do
+  def subtract({num_a, denom_a}, {num_b, denom_b}) do
+    # TODO almost identical to addition
+    {
+      (num_a * denom_b) - (num_b * denom_a),
+      denom_a * denom_b
+    }
+    |> reduce()
   end
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply(a, b) do
+  def multiply({num_a, denom_a}, {num_b, denom_b}) do
+    { num_a * num_b, denom_a * denom_b }
+    |> reduce()
   end
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, den) do
+  def divide_by({num_a, denom_a}, {num_b, denom_b}) when num_b != 0 do
+    { num_a * denom_b , num_b * denom_a }
+    |> reduce()
   end
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs(a) do
-  end
+  def abs({num, denom}), do: {Kernel.abs(num), Kernel.abs(denom)} |> reduce()
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
-  def pow_rational(a, n) do
+  def pow_rational(a, n) when n < 0, do: pow_rational(a, Kernel.abs(n))
+  def pow_rational({num, denom}, n) do
+    pow = fn(x) -> :math.pow(x, n) |> round() end
+
+    {pow.(num), pow.(denom)} |> round()
   end
 
   @doc """
@@ -54,6 +72,12 @@ defmodule RationalNumbers do
   Reduce a rational number to its lowest terms
   """
   @spec reduce(a :: rational) :: rational
-  def reduce(a) do
+  def reduce({num, denom}) do
+    gcd = gcd(num, denom)
+    { num / gcd, denom / gcd }
   end
+
+  @spec gcd(x :: integer, y :: integer) :: integer
+  defp gcd(x, 0), do: x
+  defp gcd(x, y), do: gcd(y, rem(x, y))
 end
