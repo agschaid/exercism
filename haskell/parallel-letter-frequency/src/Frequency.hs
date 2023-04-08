befc5497ad59c5ee7f9bd8620f3124703424ef53
Axel Gschaider <axel.gschaider@posteo.de>
2023-04-08 23:50:31 +0200

    minor stuff in haskell parallel letter frequency

--- haskell/parallel-letter-frequency/src/Frequency.hs
+++ haskell/parallel-letter-frequency/src/Frequency.hs
@@ -2,6 +2,7 @@ module Frequency (frequency) where
 
 import Data.Map  (Map)
 import Data.Text (Text)
+import Control.Parallel.Strategies
 
 frequency :: Int -> [Text] -> Map Char Int
 frequency nWorkers texts = error "You need to implement this function."
