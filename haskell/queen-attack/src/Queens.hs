93602efc392ee88f3000d9285b370ae17e3a1d15
Axel Gschaider <axel.gschaider@posteo.de>
2023-04-09 13:12:19 +0200

    implement haskell queen-attack

--- haskell/queen-attack/src/Queens.hs
+++ haskell/queen-attack/src/Queens.hs
@@ -1,7 +1,38 @@
 module Queens (boardString, canAttack) where
 
-boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+import Data.List
 
-canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack queenA queenB = error "You need to implement this function."
+type Column = Int
+type Line = Int
+
+data BoardElement = 
+    Square Line Column |
+    Space |
+    LineEnd
+
+boardString :: Maybe (Line, Column) -> Maybe (Line, Column) -> String
+boardString white black = map (renderWithQueens white black) emptyBoard
+
+-- an "abstract" representation of an empty board including lineends and spaces between squares.
+emptyBoard :: [BoardElement]
+emptyBoard = concat $ [ singleLine li | li <- [0..7] ]
+  where
+    singleLine li = (++ [LineEnd]) . (intersperse Space) $ [ Square li col | col <- [0..7] ]
+
+renderWithQueens :: Maybe (Line, Column) -> Maybe (Line, Column) -> BoardElement -> Char
+renderWithQueens _ _ Space = ' '
+renderWithQueens _ _ LineEnd = '\n'
+renderWithQueens white _ square | sameCoordinates white square = 'W'
+renderWithQueens _ black square | sameCoordinates black square = 'B'
+renderWithQueens _ _ (Square _ _) = '_'
+
+sameCoordinates :: Maybe(Line, Column) -> BoardElement -> Bool
+sameCoordinates (Just (liQ, colQ)) (Square liS colS) = liQ == liS && colQ == colS
+sameCoordinates _ _ = False
+
+canAttack :: (Line, Column) -> (Line, Column) -> Bool
+canAttack (liA, colA) (liB, colB) = liA == liB || colA == colB || liDist == colDist
+  where
+    -- if the line distance equals the column distance the queens are diagonal
+    liDist = abs $ liA - liB
+    colDist = abs $ colA - colB
